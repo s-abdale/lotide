aa65d781400b732f8473059909135ef28b2b7334
Safia Abdale <safia.abdale@gmail.com>
2021-11-22 21:06:36 +0000

    Function to compare two values

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("fluff", "fluff");
+assertEqual(1,"1");
+assertEqual(1, 2);
