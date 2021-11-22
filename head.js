c513253cbd5591c4626800fc8ff83e0fe3d1cccf
Safia Abdale <safia.abdale@gmail.com>
2021-11-22 22:03:22 +0000

    Formatted document with eslint

--- head.js
+++ head.js
@@ -23,4 +23,4 @@ console.log(head([])); // returns undefined */
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
-assertEqual (head(["Book"]), "Book");
+assertEqual(head(["Book"]), "Book");
