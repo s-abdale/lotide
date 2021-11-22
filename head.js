d0f7c525c8c9fa80f3912b387ad9a97cd169a2a2
Safia Abdale <safia.abdale@gmail.com>
2021-11-22 21:59:52 +0000

    Wrote head function. Added head and assertEqual to file

---
+++ head.js
@@ -0,0 +1,26 @@
+// assertEqual function:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// head function:
+const head = function(array) {
+  return array[0];
+};
+/* Testing the head function:
+console.log(head([1, 2, 3])); // returns 1
+console.log(typeof head([1, 2, 3])) // returns Number 
+console.log(head(["Book"])); // returns Book 
+console.log(head([])); // returns undefined */
+
+
+
+// Testing:
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+assertEqual (head(["Book"]), "Book");
