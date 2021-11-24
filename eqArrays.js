6e268b33e58b66b5858f95681bf58105b1b9d143
Safia Abdale <safia.abdale@gmail.com>
2021-11-24 03:42:42 +0000

    Created eqArrays function to compare 2 arrays

---
+++ eqArrays.js
@@ -0,0 +1,35 @@
+// assertEqual Function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// eqArrays Function
+let eqArrays = function(arrayA, arrayB) {
+
+  let arrayAString = arrayA.join('-');
+  let arrayBString = arrayB.join('-');
+
+  if (arrayAString === arrayBString) {
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
+}
+
+
+
+// Testing:
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+// Testing with assertEqual:
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
