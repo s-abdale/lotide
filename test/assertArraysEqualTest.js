18698a1710e6afb933e5cecf8d5a779dc6f2222d
Safia Abdale <safia.abdale@gmail.com>
2021-11-30 01:43:59 +0000

    Created test file for assertArraysEqual function

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+// test
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
