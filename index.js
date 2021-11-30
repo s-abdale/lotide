54620b39cd62710305f13b2f53492cd5c5c28977
Safia Abdale <safia.abdale@gmail.com>
2021-11-30 03:22:11 +0000

    Created an object for all utility functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual
+};
